8d7205fc8ccdbcd56ecc6e06f9184f1261c8f7d1
kevinpinzon02 <kdpinzon@unbosque.edu.co>
2022-04-03 22:15:05 -0500

    Postx2

--- src/main/java/co/edu/unbosque/taller_3/HelloServlet.java
+++ src/main/java/co/edu/unbosque/taller_3/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         // Hello
