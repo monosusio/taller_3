ed5988eb7b25ca7660a6a51a12439ff2fb56f881
santiagocm12 <scortesmo@unbosque.edu.co>
2022-04-05 19:06:44 -0500

    Funcionamiento registrar

---
+++ src/main/java/co/edu/unbosque/taller_3/SignUpServlet.java
@@ -0,0 +1,32 @@
+package co.edu.unbosque.taller_3;
+
+import co.edu.unbosque.taller_3.services.UserService;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet(name = "signup", value = "/signup")
+public class SignUpServlet extends HttpServlet {
+
+    public void init() {}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            new UserService().createUser(username, password, getServletContext().getRealPath("") + File.separator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("./index.html");
+    }
+
+    public void destroy() {}
+}
